c50b60ab1043592d0d1b7437625986899f3825e6
Abby <abbycloete3552@gmail.com>
2020-04-14 16:16:26 +1200

    first commit

--- src/components/admin/NavBarAdmin.vue
+++ src/components/admin/NavBarAdmin.vue
@@ -1,8 +1,8 @@
 <template>
   <div> 
       <ul>
-          <li> <router-link v-bind:to="'/all-items-admin'"> All Questions </router-link> </li>
-          <li> <router-link v-bind:to="'/conference-details'"> End Conference  </router-link> </li>
+          <li class="all-questions"> <router-link v-bind:to="'/all-items-admin'"> All Questions </router-link> </li>
+          <li class="end-conference"> <router-link v-bind:to="'/conference-details'"> End Conference  </router-link> </li>
       </ul>
   </div>
 </template>
@@ -10,15 +10,50 @@
 <script>
 
 export default {
-    name: "NavBarUser",
+    name: "NavBarAdmin",
 }
 </script>
 
 <style scoped>
 
-h1 {
+ul {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  background-color: white;
+}
+
+li {
+    padding: 10px 50px;
     text-align: center;
+    border: none;
+    outline: none;
+    list-style-type: none;
+}
+
+li a {
+    font-size: 1.15em;
+    text-decoration: none;
+    color: #28313f;
     font-weight: lighter;
+}
+
+.all-questions {
+    border-right: solid 1px grey;
+}
+
+.end-conference {
+    background-color: #54a9da;
+}
+
+.end-conference a {
+    color: white;
+}
+
+.router-link-exact-active {
+  font-weight: bold;
+  color: #54a9de;
 }
 
 
